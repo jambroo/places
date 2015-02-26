1c2d09b147ac55c9cedeb7939cccb6fb704290bc
James Brooking <jambroo@gmail.com>
2015-02-26 20:37:10 +0000

    Added single PHP page to handle requests and moved index.html template.

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use Slim\Slim;
+
+    $config = require 'config/main.php';
+
+    $app = new Slim();
+    $app->data = (Object) [];
+
+    // Hook to ensure places.json db is present and valid
+    $app->hook('slim.before.dispatch', function () use ($app, $config) {
+        if ($config && array_key_exists('places', $config) &&
+            file_exists($config['places'])) {
+            try {
+                $app->data->placesJSON = file_get_contents($config['places']);
+                $app->data->places = json_decode($app->data->placesJSON, true);
+
+                if (!$app->data->places) {
+                    throw new \Exception('Error reading and decoding places.json database file.');
+                }
+
+                return true;
+            } catch (\Exception $e) { 
+                throw new \Exception('Error reading and decoding places.json database file.');
+            }
+        }
+        throw new \Exception('Error reading and decoding places.json database file.');
+    });
+
+
+    // Get all
+    $app->get('/api/place', function () use ($app) {
+        echo $app->data->placesJSON;
+    });
+
+    $app->get('/api/place/:date', function ($date) use ($app, $config) {
+        if ($date && array_key_exists($date, $app->data->places)) {
+            echo $app->data->places[$date];
+        } else {
+            throw new \Exception('Error getting date entry.');
+        }
+    });
+
+    // Create new entry
+    $app->post('/api/place', function () use ($app, $config) {
+        $place = $app->request->params('place');
+        $date = $app->request->params('date');
+        
+        if ($date && place && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $date) &&
+            !array_key_exists($date, $app->data->places)) {
+            // Add new entry
+            // Can assume $app->places is valid here as before dispatch hook would have been run.
+            $places = $app->data->places;
+            $places[$date]= $place;
+            $app->data->places = $places;
+            $app->data->placesJSON = json_encode($app->data->places);
+
+            // Save file
+            if (file_put_contents($config['places'], $app->data->placesJSON)) {
+                echo '1';
+                return;
+            }
+        }
+
+        throw new \Exception('Error saving entry.');
+    });
+
+    $app->get('/:method', function () use ($app) {
+        $app->render('index.html');
+    })->conditions(array('method' => '.*?'));
+
+    $app->run();
