39c35ede5f45649cc119d4dfbb5b803f443c0e2a
James Brooking <jambroo@gmail.com>
2015-02-27 22:06:47 +0000

    Fixing date save and highlighting new entry in table after save.

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,25 @@ var placeControllers = angular.module('placeControllers', []);
 
 placeControllers.controller('PlaceCtrl', ['$scope', '$http',
   function($scope, $http) {
+    $scope.save = function(place) {
+      var yyyy = place.date.getFullYear().toString();
+      var mm = (place.date.getMonth()+1).toString();
+      var dd  = place.date.getDate().toString();
+      var date = yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+
+      var place = place.place;
+
+      $http.post('/api/place', {
+        date: date,
+        place: place
+      }).success(function(data) {
+        $scope.newEntry = date;
+        $scope.places[date] = place;
+        $scope.date = null;
+        $scope.place = null;
+      });
+    };
+
     $http.get('/api/place').success(function(data) {
 	    $scope.places = data;
 	  });
