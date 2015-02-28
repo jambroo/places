c1b2df03a6799cdabc258911ef252f934dfd734d
James Brooking <jambroo@gmail.com>
2015-02-28 09:59:47 +0000

    Adding edit form.

--- js/controllers.js
+++ js/controllers.js
@@ -29,13 +29,34 @@ placeControllers.controller('PlaceCtrl', ['$scope', '$http',
 	    $scope.places = data;
 	  });
   }]);
-placeControllers.controller('PlaceEditCtrl', ['$scope', '$routeParams', '$http', 
-  function($scope, $routeParams, $http) {
+placeControllers.controller('PlaceEditCtrl', ['$scope', '$routeParams', '$http', '$location', 
+  function($scope, $routeParams, $http, $location) {
+    var date = $routeParams.date;
+    // Could put update function here
+    $scope.update = function(place) {
+      var yyyy = place.date.getFullYear().toString();
+      var mm = (place.date.getMonth()+1).toString();
+      var dd  = place.date.getDate().toString();
+      var newDate = yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+
+      var place = place.place;
+
+      $http.put('/api/place', {
+        date: newDate,
+        place: place,
+        originalDate: date
+      }).success(function(data) {
+        $scope.success = true;
+      });
+    };
+
     var date = $routeParams.date;
     if (date.match(/^(\d{4})(\/|-)(\d{2})(\/|-)(\d{2})$/)) {
       $http.get('/api/place/'+date).success(function(data) {
-        $scope.date = new Date(date);
-        $scope.place = data;
+        $scope.place = {
+          date: new Date(date),
+          place: data
+        };
       });
     }
   }
